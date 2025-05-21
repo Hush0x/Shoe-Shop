4412b108713185ef08dc6e39fb5e3144356d2564
Hesam <mute.mood.channel@gmail.com>
2025-05-21 22:50:52 +0330

    feat : sign up complete version

---
+++ src/functions/sign-page.js
@@ -0,0 +1,87 @@
+import { headBackArrow } from '../../component/header-back-arrow';
+export function signPageGenerator(labelText, btnText, formId, page) {
+  const signPage = `<div class="w-full flex items-center h-14" id="${formId}-header"></div>
+        <img src="/svgs/logo/loginLogo.svg" alt="app logo" class="w-14 m-20" />
+        <h2 class="font-semibold text-3xl my-12">${labelText}</h2>
+        <form class="w-full flex flex-col items-center grow" id=${formId}>
+          <div class="w-full flex flex-col items-center grow">
+            <div
+              class="bg-[#FAFAFA] flex items-center gap-1 h-9 w-full rounded-b-sm relative"
+              id ="${formId}-userNameContainer"
+            >
+              <img
+                src="/svgs/input/email.svg"
+                alt="email logo"
+                class="w-4 h-4 absolute left-2"
+                id="${formId}-emailLogo"
+              />
+              <input
+                type="text"
+                name="Username"
+                id="${formId}-userName"
+                placeholder="Username"
+                class="w-full font-normal text-sm h-full pl-7"
+              />
+            </div>
+            <div class="text-left w-full">
+            <p class="text-red-500 text-xs invisible" id="${formId}-Un-error">Your username must be at least 8 characters long!</p>
+            </div>
+            <div
+              class="bg-[#FAFAFA] flex items-center h-9 w-full rounded-b-sm mt-2.5 relative"
+              id="${formId}-passwordContainer"
+            >
+              <img
+                src="/svgs/input/password.svg"
+                alt="password"
+                class="w-4 absolute left-2"
+                id="${formId}-passwordLogo"
+              />
+              <input
+                type="password"
+                name="password"
+                id="${formId}-password"
+                maxlength="20"
+                placeholder="Password"
+                class="w-full font-normal text-sm h-full pl-7"
+              />
+              <img
+                src="/svgs/input/hidden.svg"
+                alt="hidden password"
+                class="w-4 cursor-pointer absolute right-2"
+                id="${formId}-eyeLogo"
+              />
+            </div>
+            <div class="text-left w-full">
+            <p class="text-red-500 text-xs invisible" id="${formId}-password-error">Password must be stronger. Use 8+ characters with upper/lowercase, number & symbol!
+            </p>
+            </div>
+            <button type="button" class="font-medium text-sm cursor-pointer" id="${btnText}Btn">${btnText}</button>
+          </div>
+        </form>`
+  page.innerHTML = signPage
+  const header = document.getElementById(`${formId}-header`);
+  headBackArrow(header, "/welcome.html")
+  const eyeLogo = document.getElementById(`${formId}-eyeLogo`)
+  const password = document.getElementById(`${formId}-password`);
+  document.getElementById(`${formId}-userNameContainer`).addEventListener("focusin", () => {
+    document.getElementById(`${formId}-emailLogo`).src = "/svgs/input/focusedEmail.svg"
+  })
+  document.getElementById(`${formId}-passwordContainer`).addEventListener("focusin", () => {
+    document.getElementById(`${formId}-passwordLogo`).src = "/svgs/input/focusedPassword.svg"
+    if (password.type === "password") {
+      eyeLogo.src = "/svgs/input/focusedHidden.svg"
+    } else {
+      eyeLogo.src = "/svgs/input/showPassword.svg";
+    }
+  })
+  document.getElementById(`${formId}-eyeLogo`).addEventListener("click", () => {
+    if (password.type === "password") {
+      password.type = "text";
+      eyeLogo.src = "/svgs/input/showPassword.svg";
+    } else {
+      password.type = "password";
+      eyeLogo.src = "/svgs/input/focusedHidden.svg";
+    }
+  });
+}
+
---
+++ src/pages/signup-page.js
@@ -0,0 +1,85 @@
+import { signPageGenerator } from '../functions/sign-page';
+import { generateAppButton } from '../../component/app-button';
+import { signupAccount } from '../../apis/auth';
+import { tokenName } from '../../apis/auth-token';
+const signupContainer = document.getElementById("signupContainer")
+const signupBtn = generateAppButton("Signup")
+signupBtn.type = "submit"
+signupBtn.classList.remove("bg-[#212529]", "cursor-pointer")
+signupBtn.classList.add("bg-[#6E7174]", "cursor-not-allowed")
+signupBtn.disabled = true
+signupBtn.classList.remove("bg-[#212529]")
+signupBtn.classList.add("bg-[#6E7174]")
+signupBtn.disabled = true
+signPageGenerator("Signup to Your Account", "login", "signUpForm", signupContainer)
+const goToLogin = document.getElementById("loginBtn")
+const signupForm = document.getElementById("signUpForm")
+const upperCase = /[A-Z]/;
+const lowerCase = /[a-z]/;
+const specialChar = /[@!$.*\-_/%^&#~]/;
+const numbers = /[0-9]/;
+const signupPasswordInput = document.getElementById("signUpForm-password")
+const signupFormUnInput = document.getElementById("signUpForm-userName")
+signupForm.appendChild(signupBtn)
+goToLogin.addEventListener("click", () => {
+    signupForm.reset()
+    window.location.href = "/login.html";
+})
+signupPasswordInput.addEventListener("input", () => {
+    const passCheck = signupPasswordInput.value;
+    if ((passCheck.length >= 8) &&
+        (upperCase.test(passCheck)) &&
+        (lowerCase.test(passCheck)) &&
+        (specialChar.test(passCheck)) &&
+        (numbers.test(passCheck))) {
+        document.getElementById("signUpForm-password-error").classList.add("invisible")
+    }
+    else {
+        document.getElementById("signUpForm-password-error").classList.remove("invisible")
+    }
+})
+signupFormUnInput.addEventListener("input", () => {
+    const unCheck = signupFormUnInput.value;
+    if ((unCheck.length >= 5)) document.getElementById("signUpForm-Un-error").classList.add("invisible")
+    else document.getElementById("signUpForm-Un-error").classList.remove("invisible")
+})
+
+
+signupForm.addEventListener("input", () => {
+    const passCheck = signupPasswordInput.value;
+    const unCheck = signupFormUnInput.value;
+    if (
+        (passCheck.length >= 8) &&
+        (upperCase.test(passCheck)) &&
+        (lowerCase.test(passCheck)) &&
+        (specialChar.test(passCheck)) &&
+        (unCheck.length >= 5) &&
+        (numbers.test(passCheck))
+    ) {
+        signupBtn.classList.add("bg-[#212529]", "cursor-pointer")
+        signupBtn.classList.remove("bg-[#6E7174]", "cursor-not-allowed")
+        signupBtn.disabled = false
+    } else {
+        signupBtn.classList.remove("bg-[#212529]", "cursor-pointer");
+        signupBtn.classList.add("bg-[#6E7174]", "cursor-not-allowed");
+        signupBtn.disabled = true;
+    }
+})
+signupForm.addEventListener("submit", async (event) => {
+    const passCheck = signupPasswordInput.value;
+    const unCheck = signupFormUnInput.value;
+    const data = { username: unCheck, password: passCheck };
+    localStorage.setItem("username", data.username)
+
+    event.preventDefault();
+    try {
+        const res = await signupAccount(data)
+        localStorage.setItem(tokenName, res.data.token)
+
+        signupForm.reset()
+        window.location.href = "/home.html";
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
