45068f5aa016ff2ba1f9eb8ea23d6663fdb6599d
Hesam <mute.mood.channel@gmail.com>
2025-05-21 22:55:28 +0330

    feat : filter component complete version

---
+++ component/filter-button.js
@@ -0,0 +1,20 @@
+export function generateFilter(name, target) {
+    const filterBtn = document.createElement("div")
+    filterBtn.className = "w-fit px-5 h-10 flex items-center text-[#343A40] bg-white rounded-3xl border-2 border-[#343A40] font-semibold text-base m-2 whitespace-nowrap filter"
+    filterBtn.textContent = name
+    if (name === "All") {
+        filterBtn.classList.remove("text-[#343A40]", "bg-white")
+        filterBtn.classList.add("bg-[#343A40]", "text-white")
+    }
+    filterBtn.addEventListener("click", () => {
+        const allFilters = target.querySelectorAll("div")
+        allFilters.forEach(box => {
+            box.classList.add("text-[#343A40]", "bg-white")
+            box.classList.remove("bg-[#343A40]", "text-white")
+        })
+        filterBtn.classList.remove("text-[#343A40]", "bg-white")
+        filterBtn.classList.add("bg-[#343A40]", "text-white")
+    })
+    target.appendChild(filterBtn)
+    return filterBtn
+}
