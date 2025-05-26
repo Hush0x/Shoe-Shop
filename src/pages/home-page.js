7864dd6b917f1a1d9528fddcd9259336f9fd9c20
Hesam <mute.mood.channel@gmail.com>
2025-05-26 21:23:16 +0330

    home page edited version

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -6,20 +6,23 @@ import { loadProducts } from '../functions/load-product';
 // elements
 let page = 1
 const limit = 10
-let totalPage = 0
+let totalPage = 5
 const greetingContainer = document.getElementById("greeting")
 const greetingValue = getGreeting()
 const filterContainer = document.getElementById("filterContainer")
+const searchInput = document.getElementById("searchInput")
 const notificationContainer = document.getElementById("notifContainer")
 const footerContainer = document.getElementById("footerContainer")
 const productContainer = document.getElementById("productContainer")
 const userName = document.createElement("p")
-const logOut = document.createElement("a")
-logOut.href = "/login.html"
+let isInfiniteScrollActive = true;
+// logout
+const logOut = document.createElement("p")
 logOut.className = "cursor-pointer"
 logOut.textContent = "Logout"
 logOut.addEventListener("click", () => {
   localStorage.clear()
+  location.reload();
 });
 userName.textContent = localStorage.getItem("username")
 // generate footer
@@ -28,36 +31,45 @@ footerGenerator(footerContainer)
 greetingContainer.innerText = greetingValue
 greetingContainer.appendChild(logOut)
 notificationContainer.appendChild(userName)
-// filter
-const filterAll = generateFilter("All", filterContainer)
-filterAll.addEventListener("click", () => {
-  productContainer.querySelectorAll("div").forEach(div => {
-    div.classList.remove("hidden");
-  })
-})
-const { data: filterResponse } = await getShoesFilter()
-filterResponse.forEach((item) => {
-  const x = generateFilter(item, filterContainer);
-  x.addEventListener("click", () => {
-    const products = Array.from(productContainer.children)
-    let result = products.filter((element) => element.getAttribute("brand") === item);
-    productContainer.querySelectorAll(".card").forEach(div => {
-      div.classList.add("hidden");
-    });
-    products
-      .filter(p => p.getAttribute("brand") === item)
-      .forEach(p => p.classList.remove("hidden"));
-  });
-});
 // shoes
 let shoesResponse = await getShoes(`?page=${page}&limit=${limit}`);
 totalPage = shoesResponse.data.totalPages;
-console.log(totalPage);
 shoesResponse.data.data.forEach(item => {
   loadProducts(productContainer, item);
 });
+
+// filter all
+const filterAll = generateFilter("All", filterContainer)
+filterAll.addEventListener("click", async () => {
+  isInfiniteScrollActive = true;
+  productContainer.innerHTML = ""
+  shoesResponse.data.data.forEach(item => {
+    loadProducts(productContainer, item);
+  });
+})
+// filter shoes
+const { data: filterResponse } = await getShoesFilter()
+filterResponse.forEach((item) => {
+  const brand = item
+  const filterBtn = generateFilter(item, filterContainer);
+  filterBtn.addEventListener("click", async () => {
+    isInfiniteScrollActive = false;
+    productContainer.innerHTML = "";
+    for (let i = 1; i <= totalPage; i++) {
+      let brandResponse = await getShoes(`?page=${i}&limit=${limit}`);
+      const filter = brandResponse.data.data
+      const brandFilter = filter.filter(x => x.brand === brand);
+      console.log(i);
+      console.log(brandFilter);
+      brandFilter.forEach(product => {
+        loadProducts(productContainer, product);
+      });
+    }
+  });
+});
 productContainer.addEventListener("scroll", async () => {
-  if (window.scrollY + window.innerHeight + 300 >= document.documentElement.scrollHeight && page < totalPage) {
+  if (!isInfiniteScrollActive) return;
+  if (productContainer.scrollTop + productContainer.clientHeight >= productContainer.scrollHeight - 200 && page < totalPage) {
     page++;
     const newShoes = await getShoes(`?page=${page}&limit=${limit}`);
     newShoes.data.data.forEach(item => {
@@ -65,4 +77,7 @@ productContainer.addEventListener("scroll", async () => {
     });
   }
 });
-// 
+// search
+searchInput.addEventListener("focusin", () => {
+  location.href = `/search`
+})
